8f79417529e49e0c77758f2423675e91bc111fe8
carlyd95 <dcarltondunlap@me.com>
2014-11-26 19:42:58 -0500

    dfd
    dfsdf

--- repo/_extras_/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$server = "localhost"; 
-$user = "yourusername"; 
-$password = "yourpassword"; 
-$database = mysql_connect ($server, $user, $password); 
-mysql_select_db("yourdatabase", $database); 
-
-?>
--- repo/_extras_/createtable.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `download` (
-	`filename` varchar(255) NOT NULL,
-	`dldate` DATETIME,
-	`stats` int(11) NOT NULL,
-	PRIMARY KEY  (`filename`)
-)
