43e82bcffe39b62a3146bfca264cca04d45886ed
carlyd95 <dcarltondunlap@hotmail.com>
2018-09-27 23:59:06 -0400

    27/09

--- carltons-karoo/uploadpics.py
+++ carltons-karoo/uploadpics.py
@@ -2,6 +2,18 @@
 
 import os
 
+Aizocaceae = ['lithops']
+Asphodelaceae = ['Aloe', 'Astroloba', 'Gasteria', 'Haworthia', 'Haworthiopsia', 'Tulista']
+Cactaceae = ['Ariocarpus', 'Astrophytum', 'Frailea', 'Gymnocalycium', 'Lophophora', 'Melocactus']
+Euphorbiaceae = ['Euphorbia']
+Hyacinthaceae = ['Schizobasis', 'Boweia']
+Asclepiadaceae = ['Huernia', 'Stapelia', 'Orbea', 'Duvalia', 'Rhytidocaulon', 'Trichocaulon']
+Portulacaceae = ['Avonia', 'Crassula']
+
+family = {'a':Aizocaceae, 'b':Asphodelaceae, 'c':Cactaceae, 'd':Euphorbiaceae, 'e':Hyacinthaceae, 'f':Asclepiadaceae, 'g':Portulacaceae}
+
+
+
 for line in (os.listdir("/Users/carlyd95/git/carltons-karoo/pics")):
 		if line.endswith(('.jpg')):
-			
+			list line.replace('.', '')
