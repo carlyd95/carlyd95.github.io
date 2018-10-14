6f7c2560dbdc273a555a60013d195ae6808ea31f
carlyd95 <dcarltondunlap@hotmail.com>
2018-10-14 13:30:58 -0400

    14/10

--- carltons-karoo/update.py
+++ carltons-karoo/update.py
@@ -42,6 +42,9 @@ hya = ['Adenotheca', 'Albuca', 'Basaltogeton', 'Bowiea', 'Caloscilla', 'Charybdi
 por = ['Anacampseros', 'Avonia', 'Calandrinia', 'Ceraria', 'Cistanthe', 'Claytonia', 'Grahamia', 'Helianthemoides', 'Lewisia', 'Oreobroma', 'Phemeranthus', 'Portulaca', 'Portulacaria', 'Ruelingia', 'Talinum']
 cac = ['Acanthanthus', 'Acanthocalycium', 'Acanthocephala', 'Acanthocereus', 'Acantholobivia', 'Acanthopetalus', 'Acanthorhipsalis', 'Acharagma', 'Airampoa', 'Akersia', 'Ancistrocactus', 'Andenea', 'Andinopuntia', 'Anhalonium', 'Anisocereus', 'Aparadoa', 'Aporocactus', 'x Aporoheliocereus', 'Arequipa', 'Arequipiopsis', 'Ariocarpus', 'Armatocereus', 'Arrojadoa', 'Arrojadoopsis', 'Arthrocereus', 'Arthrophyllum', 'Astrobergia', 'Astrophytum', 'Aulacothele', 'Austrocactus', 'Austrocephalocereus', 'Austrocylindropuntia', 'Aylostera', 'Aztekium', 'Azureocereus', 'Backebergia', 'Banfiopuntia', 'Bartschella', 'Bergerocactus', 'Bergerocereus', 'Binghamia', 'Bisnaga', 'Blossfeldia', 'Bolivicactus', 'Bolivicereus', 'Bonifazia', 'Borzicactella', 'Borzicactus', 'x Borzipostoa', 'Brachycalycium', 'Brachycereus', 'Bragaia', 'Brasilicactus', 'Brasilicereus', 'Brasiliopuntia', 'Brasiliparodia', 'Brasilocactus', 'Bravocactus', 'Bridgesia', 'Brittonia', 'Brittonrosea', 'Browningia', 'Buiningia', 'Cactodendron', 'Cactus', 'Calymmanthium', 'Carnegiea', 'Carpophillus', 'Cassyta', 'Cassytha', 'Castellanosia', 'Cephalocereus', 'Cephalocleistocactus', 'Cephalomamillaria', 'Cephalophorus', 'Ceratistes', 'Cereus', 'Chaffeyopuntia', 'Chamaecereus', 'x Chamaelobivia', 'Chiapasia', 'Chichipia', 'Chilenia', 'Chileniopsis', 'Chileocactus', 'Chileorebutia', 'Chilita', 'Chrysocactus', 'Cinnabarinea', 'Cintia', 'Cipocereus', 'Cirinosum', 'Clavarioidia', 'Clavatopuntia', 'Cleistocactus', 'Cleistocereus', 'Clistanthocereus', 'Cochemiea', 'Cochiseia', 'Coleocephalocereus', 'Coloradoa', 'Consolea', 'Copiapoa', 'Corryocactus', 'Corryocereus', 'Corynopuntia', 'Coryphantha', 'Cryptocereus', 'Cullmannia', 'Cumarinia', 'Cumulopuntia', 'Cylindrolobivia', 'Cylindropuntia', 'Cylindrorebutia', 'Dactylanthocactus', 'Deamia', 'Delaetia', 'Dendrocereus', 'Denmoza', 'Digitorebutia', 'Digitostigma', 'Diploperianthium', 'Discocactus', 'Disisocactus', 'Disisorhipsalis', 'Disocactus', 'Disocereus', 'Dolichothele', 'Ebnerella', 'Eccremocactus', 'Echinocactus', 'Echinocereus', 'x Echinoferocactus', 'Echinofossulocactus', 'Echinolobivia', 'Echinomastus', 'Echinonyctanthus', 'Echinopsis', 'Echinorebutia', 'Efossus', 'Emorycactus', 'Encephalocarpus', 'Eomatucana', 'x Epinicereus', 'Epiphyllanthus', 'Epiphyllopsis', 'Epiphyllum', 'Epithelantha', 'Erdisia', 'Eriocactus', 'Eriocephala', 'x Eriocereopsis', 'Eriocereus', 'Eriosyce', 'Erythrocereus', 'Erythrorhipsalis', 'Escobaria', 'Escobariopsis', 'Escobesseya', 'Escobrittonia', 'Escocoryphantha', 'Escontria', 'Espostoa', 'x Espostocactus', 'Espostoopsis', 'Estevesia', 'Eulychnia', 'Euporteria', 'Eurebutia', 'Facheiroa', 'x Ferenocactus', 'x Ferobergia', 'Ferocactus', 'x Ferofossulocactus', 'Floribunda', 'Fobea', 'Frailea', 'Friesia', 'Furiolobivia', 'Geohintonia', 'Gerocephalus', 'Glandulicactus', 'Glandulifera', 'Grusonia', 'Gymnantha', 'Gymnanthocereus', 'Gymnocactus', 'Gymnocalycium', 'Gymnocereus', 'Gymnorebutia', 'Haagea', 'Haageocereus', 'x Haagespostoa', 'Hamatocactus', 'Hariota', 'Harrisia', 'x Harrisinopsis', 'Haseltonia', 'Hatiora', 'Heliabravoa', 'Helianthocereus', 'x Heliaporus', 'Heliocereus', 'Hertrichocereus', 'Hickenia', 'Hildewintera', 'Hildmannia', 'Homalocephala', 'Horridocactus', 'Hylocereus', 'Hylorhipsalis', 'Hymenolobivia', 'Hymenorebutia', 'Islaya', 'Isolatocereus', 'Jasminocereus', 'Kadenicarpus', 'Krainzia', 'Lactomamillaria', 'Lagenosocereus', 'Lasiocereus', 'Lemaireocereus', 'Leocereus', 'Lepidocoryphantha', 'Lepismium', 'Leptocereus', 'Leptocladia', 'Leptocladodia', 'Leuchtenbergia', 'Leucostele', 'Leuenbergeria', 'Lobeira', 'Lobivia', 'Lobiviopsis', 'Lodia', 'Lophocereus', 'Lophophora', 'Loxanthocereus', 'Lymanbensonia', 'Machaerocereus', 'Maierocactus', 'Maihuenia', 'Maihueniopsis', 'Malacocarpus', 'Mamillopsis', 'Mammillaria', 'Mammilloydia', 'Marenopuntia', 'Marginatocereus', 'Maritinocereus', 'Marniera', 'Marshallocereus', 'Masarykia', 'Matucana', 'Mediocactus', 'Mediolobivia', 'Mediorebutia', 'Melocactus', 'Mesechinopsis', 'Meyenia', 'Meyerocactus', 'Micranthocereus', 'Micropuntia', 'Microspermia', 'Mila', 'Miqueliopuntia', 'Mirabella', 'Mitrocereus', 'Monvillea', 'Morangaya', 'Morawetzia', 'x Myrtgerocactus', 'Myrtillocactus', 'x Myrtillocalycium', 'Myrtillocereus', 'Napina', 'Navajoa', 'Neoabbottia', 'Neobesseya', 'x Neobinghamia', 'Neobuxbaumia', 'Neocardenasia', 'Neochilenia', 'Neodawsonia', 'Neodiscocactus', 'Neoevansia', 'Neogomesia', 'Neohickenia', 'Neolemaireocereus', 'Neolloydia', 'Neolobivia', 'Neomammillaria', 'Neomatucana', 'Neonavajoa', 'Neoporteria', 'Neoraimondia', 'Neotanahashia', 'Neowerdermannia', 'Nichelia', 'Nopalea', 'Nopalxochia', 'Normanbokea', 'Nothorhipsalis', 'Notocactus', 'Nyctocereus', 'Obregonia', 'Oehmea', 'Ophiorhipsalis', 'Opuntia', 'Opuntiopsis', 'Oreocereus', 'Oroya', 'Ortegocactus', 'x Ortegopuntia', 'x Pacherocactus', 'x Pachgerocereus', 'Pachycereus', 'Parodia', 'Parrycactus', 'Parviopuntia', 'Pediocactus', 'Pelecyphora', 'Peniocereus', 'Pereskia', 'Pereskiopsis', 'Peronocactus', 'Peruvocereus', 'Peyotl', 'Pfeiffera', 'Phellosperma', 'Philippicereus', 'Phyllarthus', 'Phyllocactus', 'Pierrebraunia', 'Pilocanthus', 'Pilocereus', 'Pilocopiapoa', 'Pilosocereus', 'Piptanthocereus', 'Platyopuntia', 'Polaskia', 'Porfiria', 'Praecereus', 'Pseudoacanthocereus', 'Pseudoechinopsis', 'Pseudoespostoa', 'Pseudolobivia', 'Pseudomaihueniopsis', 'Pseudomammillaria', 'Pseudomitrocereus', 'Pseudonopalxochia', 'Pseudopilocereus', 'Pseudorhipsalis', 'Pseudoselenicereus', 'Pseudosolisia', 'Pseudotephrocactus', 'Pseudozygocactus', 'Pterocactus', 'Pterocereus', 'Puebloa', 'Puna', 'Punotia', 'Pygmaeocereus', 'Pyrrhocactus', 'Quiabentia', 'Rapicactus', 'Rathbunia', 'Rauhocereus', 'Rebulobivia', 'Rebutia', 'Reicheocactus', 'Rhipsalidopsis', 'Rhipsalis', 'Rhipsaphyllopsis', 'Rhodocactus', 'Rimacactus', 'Ritterocactus', 'Ritterocereus', 'Rodentiophila', 'Rooksbya', 'Roseocactus', 'Roseocereus', 'Rowleyara', 'Salmonopuntia', 'Salpingolobivia', 'Samaipaticereus', 'Schlumbergera', 'Sclerocactus', 'Scoparebutia', 'Selenicereus', 'x Seleniphyllum', 'Sericocactus', 'Seticereus', 'Seticleistocactus', 'Setiechinopsis', 'Setirebutia', 'Siccobaccatus', 'Soehrensia', 'Solisia', 'Spegazzinia', 'Sphaeropuntia', 'Spinicalycium', 'Stenocactus', 'Stenocereus', 'x Stenogonia', 'Stephanocereus', 'Stetsonia', 'Strombocactus', 'Strophocactus', 'Submatucana', 'Subpilocereus', 'Sulcorebutia', 'Tacinga', 'Tephrocactus', 'x Thelobergia', 'Thelocactus', 'Thelocephala', 'Thelomastus', 'Thrixanthocereus', 'Torreycactus', 'Toumeya', 'Trichocereus', 'x Trichopsis', 'Tunas', 'Tunilla', 'Turbinicarpus', 'Uebelmannia', 'Ursopuntia', 'Utahia', 'Vatricania', 'Weberbauerocereus', 'Weberiopuntia', 'Weberocereus', 'Weingartia', 'Werckleocereus', 'Wigginsia', 'Wilcoxia', 'Wilmattea', 'Winteria', 'Winterocereus', 'Wittia', 'Wittiocactus', 'Yavia', 'Yungasocereus', 'Zehntnerella', 'Zygocactus', 'Zygocereus']
 
+
+all = asp + asc + aiz + eup + cra + hya + por + cac
+
 cac = ['acs', 'acm', 'aca', 'acs', 'aca', 'acs', 'acs', 'aca', 'aia', 'aka', 'ans', 'ana', 'ana', 'anm', 'ans', 'apa', 'aps', 'xas', 'ara', 'ars', 'ars', 'ars', 'ara', 'ars', 'ars', 'arm', 'asa', 'asm', 'aue', 'aus', 'aus', 'aua', 'aya', 'azm', 'azs', 'baa', 'baa', 'baa', 'bes', 'bes', 'bia', 'bia', 'bla', 'bos', 'bos', 'boa', 'boa', 'bos', 'xba', 'brm', 'brs', 'bra', 'brs', 'brs', 'bra', 'bra', 'brs', 'brs', 'bra', 'bra', 'bra', 'bra', 'bua', 'can', 'cas', 'cam', 'caa', 'cas', 'caa', 'caa', 'caa', 'ces', 'ces', 'cea', 'ces', 'ces', 'ces', 'cha', 'chs', 'xca', 'cha', 'cha', 'cha', 'chs', 'chs', 'cha', 'cha', 'chs', 'cia', 'cia', 'cis', 'cim', 'cla', 'cla', 'cls', 'cls', 'cls', 'coa', 'coa', 'cos', 'coa', 'coa', 'coa', 'cos', 'cos', 'coa', 'coa', 'crs', 'cua', 'cua', 'cua', 'cya', 'cya', 'cya', 'das', 'dea', 'dea', 'des', 'dea', 'dia', 'dia', 'dim', 'dis', 'dis', 'dis', 'dis', 'dis', 'doe', 'eba', 'ecs', 'ecs', 'ecs', 'xes', 'ecs', 'eca', 'ecs', 'ecs', 'ecs', 'eca', 'efs', 'ems', 'ens', 'eoa', 'xes', 'eps', 'eps', 'epm', 'epa', 'era', 'ers', 'era', 'xes', 'ers', 'ere', 'ers', 'ers', 'esa', 'ess', 'esa', 'esa', 'esa', 'esa', 'esa', 'xes', 'ess', 'esa', 'eua', 'eua', 'eua', 'faa', 'xfs', 'xfa', 'fes', 'xfs', 'fla', 'foa', 'fra', 'fra', 'fua', 'gea', 'ges', 'gls', 'gla', 'gra', 'gya', 'gys', 'gys', 'gym', 'gys', 'gya', 'haa', 'has', 'xha', 'has', 'haa', 'haa', 'xhs', 'haa', 'haa', 'hea', 'hes', 'xhs', 'hes', 'hes', 'hia', 'hia', 'hia', 'hoa', 'hos', 'hys', 'hys', 'hya', 'hya', 'isa', 'iss', 'jas', 'kas', 'kra', 'laa', 'las', 'las', 'les', 'les', 'lea', 'lem', 'les', 'lea', 'lea', 'lea', 'lee', 'lea', 'loa', 'loa', 'los', 'loa', 'los', 'loa', 'los', 'lya', 'mas', 'mas', 'maa', 'mas', 'mas', 'mas', 'maa', 'maa', 'maa', 'mas', 'mas', 'maa', 'mas', 'maa', 'maa', 'mes', 'mea', 'mea', 'mes', 'mes', 'mea', 'mes', 'mis', 'mia', 'mia', 'mia', 'mia', 'mia', 'mis', 'moa', 'moa', 'moa', 'xms', 'mys', 'xmm', 'mys', 'naa', 'naa', 'nea', 'nea', 'xna', 'nea', 'nea', 'nea', 'nea', 'nes', 'nea', 'nea', 'nea', 'nes', 'nea', 'nea', 'nea', 'nea', 'nea', 'nea', 'nea', 'nea', 'nea', 'nia', 'noa', 'noa', 'noa', 'nos', 'nos', 'nys', 'oba', 'oea', 'ops', 'opa', 'ops', 'ors', 'ora', 'ors', 'xoa', 'xps', 'xps', 'pas', 'paa', 'pas', 'paa', 'pes', 'pea', 'pes', 'pea', 'pes', 'pes', 'pes', 'pel', 'pfa', 'pha', 'phs', 'phs', 'phs', 'pia', 'pis', 'pis', 'pia', 'pis', 'pis', 'pla', 'poa', 'poa', 'prs', 'pss', 'pss', 'psa', 'psa', 'pss', 'psa', 'pss', 'psa', 'pss', 'pss', 'pss', 'psa', 'pss', 'pss', 'pts', 'pts', 'pua', 'pua', 'pua', 'pys', 'pys', 'qua', 'ras', 'raa', 'ras', 'rea', 'rea', 'res', 'rhs', 'rhs', 'rhs', 'rhs', 'ris', 'ris', 'ris', 'roa', 'roa', 'ros', 'ros', 'roa', 'saa', 'saa', 'sas', 'sca', 'scs', 'sca', 'ses', 'xsm', 'ses', 'ses', 'ses', 'ses', 'sea', 'sis', 'soa', 'soa', 'spa', 'spa', 'spm', 'sts', 'sts', 'xsa', 'sts', 'sta', 'sts', 'sts', 'sua', 'sus', 'sua', 'taa', 'tes', 'xta', 'ths', 'tha', 'ths', 'ths', 'tos', 'toa', 'trs', 'xts', 'tus', 'tua', 'tus', 'uea', 'ura', 'uta', 'vaa', 'wes', 'wea', 'wes', 'wea', 'wes', 'wia', 'wia', 'wia', 'wia', 'wis', 'wia', 'wis', 'yaa', 'yus', 'zea', 'zys', 'zys']
 
 asp = ['ale', 'als', 'aln', 'ala', 'xaa', 'apa', 'are', 'asa', 'xaa', 'buo', 'caa', 'xge', 'xga', 'gaa', 'xga', 'gua', 'haa', 'has', 'kua', 'kua', 'lea', 'lee', 'pan', 'poa', 'pts', 'rhm', 'tua']
@@ -93,43 +96,309 @@ dic={}
 def imageLinker(family):
 	for line in (os.listdir(os.environ['HOME'] + "git/carltons-karoo/pics/plants/" + family))
 		f.write()
-fam={"e":"Euphorbiaceae"}
+
+
+
+
+
+"""
+Asclepiadaceae | 10 seeds
+Hyacinthaceae 10 seeds
+Asphodelaceae | 10 seeds
+Crassulaceae
+Aizoaceae | 25 seeds
+Cactaceae | 10 seeds
+Portulacaceae
+Euphorbiaceae | 5 seeds
+"""
+
+
+
+
+def orgpics(filename, form):
+	form[filename[:3]].append(filename[4:])
+
+
+
+aseeds= """<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="content-type"
+content="text/html;charset=utf-8" />
+		<title>
+		CK: Seeds
+		</title>
+		<meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=0">
+		
+		<link rel="stylesheet" type="text/css" href="../depics/css/layoutNew.css">
+	</head>
+
+	<body>
+		<div class="outer">
+		<div class="middle">
+		<div id="container">
+
+			<div id="myNav" class="overlay">
+			<h1 class="center">Page Navigation</h1>
+		  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+		  <div class="overlay-content">
+		  	<hr>
+		    <a href="/">Home</a>
+		    <hr>
+		    <a href="/carltons-karoo/seeds.html">Seeds</a>
+		     <hr>
+		    <a href="/carltons-karoo/plants.html">Plants </a>
+		    <hr>
+		    <a href="/carltons-karoo/cultivation.html">Succulent Cultivation Notes</a>
+
+		    <hr>
+		    <a href="/homebrew/index.html">Homebrew</a>
+		    <hr>
+		  </div>
+		</div>
+
+			<div id="contentSingleTop">
+				<img src="../depics/images/seeds.jpg" 
+				class="croppedImage">
+				<h2 class="repoDescriptionHeader">Seeds<br>For Sale</h2>
+
+				<span style="font-size:35px;cursor:pointer" class="right" onclick="openNav()">&#9776;</span>
+
+				<script>
+				function openNav() {
+				  document.getElementById("myNav").style.width = "100%";
+				}
+
+				function closeNav() {
+				  document.getElementById("myNav").style.width = "0%";
+				}
+				</script>
+			
+			</div><!--Close Package Header Div-->
+			<div id="contentSingle">
+				<h3 class="textHeaderLabel">Order Information</h3>
+			</div>
+			<div id="contentBottom">
+				<ul>
+					<li>
+						<p>For the time being U.S. orders only...
+								<br>
+								<br>
+							The shipping is $2 for up to 6 packs of seeds.  $0.50 for each additional 4 packs.  Seed amount per pack differs according to familial rank and is displayed to the right of each botanical family.  If seeds are the product of unknown hybridization the mother plant will be displayed. 
+						</p>
+					</li> 
+				</ul> 
+			</div>
+
+					 <div id="contentSingle">
+						     <h3 class="textHeaderLabel">"""
+ofam = """</h3>
+					 </div>
+					 <div id="contentBottom">
+							<ul>"""
+oseeds="""</div>
+					</li>
+				</ul>			
+			</div><!--Close Package Header Div-->
+
+		</div><!--Close Container Div-->
+		</div>
+		</div>
+	</body>
+	
+</html>
+"""
+
+aentry="""
+								<li>
+									<a href=""""
+
+
+seeds={'form':['seeds'],'asp':[],'asc':[],'aiz':[],'eup':[],'cra':[],'hya':[],'por':[],'cac':[]}
+plants={'form':['plants'],'asp':[],'asc':[],'aiz':[],'eup':[],'cra':[],'hya':[],'por':[],'cac':[]}
+
+
+
+dirx=os.environ['HOME'] +'/git/carltons-karoo/'
+begin="""<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="content-type"
+content="text/html;charset=utf-8" />
+		<title>
+		CK: Seeds
+		</title>
+		<meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=0">
+		
+		<link rel="stylesheet" type="text/css" href="../depics/css/layoutNew.css">
+	</head>
+
+	<body>
+		<div class="outer">
+		<div class="middle">
+		<div id="container">
+
+			<div id="myNav" class="overlay">
+			<h1 class="center">Page Navigation</h1>
+		  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+		  <div class="overlay-content">
+		  	<hr>
+		    <a href="/">Home</a>
+		    <hr>
+		    <a href="/carltons-karoo/seeds.html">Seeds</a>
+		     <hr>
+		    <a href="/carltons-karoo/plants.html">Plants </a>
+		    <hr>
+		    <a href="/carltons-karoo/cultivation.html">Succulent Cultivation Notes</a>
+
+		    <hr>
+		    <a href="/homebrew/index.html">Homebrew</a>
+		    <hr>
+		  </div>
+		</div>
+
+			<div id="contentSingleTop">
+				<img src="../depics/images/seeds.jpg" 
+				class="croppedImage">
+				<h2 class="repoDescriptionHeader">Seeds<br>For Sale</h2>
+
+				<span style="font-size:35px;cursor:pointer" class="right" onclick="openNav()">&#9776;</span>
+
+				<script>
+				function openNav() {
+				  document.getElementById("myNav").style.width = "100%";
+				}
+
+				function closeNav() {
+				  document.getElementById("myNav").style.width = "0%";
+				}
+				</script>
+			
+			</div><!--Close Package Header Div-->
+			<div id="contentSingle">
+				<h3 class="textHeaderLabel">Order Information</h3>
+			</div>
+			<div id="contentBottom">
+				<ul>
+					<li>
+						<p>For the time being U.S. orders only...
+								<br>
+								<br>
+							The shipping is $2 for up to 6 packs of seeds.  $0.50 for each additional 4 packs.  Seed amount per pack differs according to familial rank and is displayed to the right of each botanical family.  If seeds are the product of unknown hybridization the mother plant will be displayed. 
+						</p>
+					</li> 
+				</ul> 
+			</div>
+"""
+
+seeds={'form':['seeds'],'asp':['Asphodelaceae | 10 seeds'],'asc':['Asclepiadaceae | 10 seeds'],'aiz':['Aizoaceae | 25 seeds'],'eup':['Euphorbiaceae | 5 seeds'],'cra':['Crassulaceae | 10 seeds'],'hya':['Hyacinthaceae 10 seeds'],'por':['Portulacaceae | 10 seeds'],'cac':['Cactaceae | 10 seeds']}
+plants={'form':['seeds'],'asp':['Asphodelaceae | 10 seeds'],'asc':['Asclepiadaceae | 10 seeds'],'aiz':['Aizoaceae | 25 seeds'],'eup':['Euphorbiaceae | 5 seeds'],'cra':['Crassulaceae | 10 seeds'],'hya':['Hyacinthaceae 10 seeds'],'por':['Portulacaceae | 10 seeds'],'cac':['Cactaceae | 10 seeds']}
+
+
+def orgpics(form):
+	for line in (os.listdir(dirx + 'pics/' + form['form'][0])):
+		if line != '.DS_Store':
+			form[line[:3]].append(line)
+
+def sortnew(dic):
+	full=[]
+	for key in dic:
+		if len(dic[key]) > 1:
+			full.append(key)
+			full.sort()
+	return full
+
 def decy(filename):
-	list=filename.replace("-", "\n").split()
-	dic[filename]=filename.replace(".JPG", "").replace("$", "\n").replace(list[0] + "-", fam.get(list[0])).replace("-", "\n")
+	splitname=filename.replace('.JPG', '').split('!')
+	splitname[1] = splitname[1][:1].upper() + splitname[1][1:]
+	splitname[3] = '$' + splitname[3]
+	splitname.append(filename)
+	return splitname
+
+fambegin='''<div id="contentSingle">
+						     <h3 class="textHeaderLabel">'''
+titleend='''</h3>
+					 </div>
+					 <div id="contentBottom">
+					 	<ul>'''
+famend='''</ul>
+					</div>'''
+
+#def addtosite():
+s=open((dirx + 'seeds.html'), 'w+')
+linkfix='pics/seeds/'
+s.write(begin)
+for key in sortnew(seeds):
+	s.write( fambegin + seeds[key][0] + titleend)
+	x = decy(seeds[key][1])
+	s.write('<a href="' + (linkfix + x[4].replace('"', '&#34;')) + '">' + (x[1] + ' ' + x[2]) + '</a> ' + x[3])
+	for index in seeds[key]:
+		if seeds[key].index(index) > 1:
+			x = decy(index)
+			s.write('<hr>\n<a href="' + (linkfix + x[4].replace('"', '&#34;')) + '">' + (x[1] + ' ' + x[2]) + '</a> ' + x[3])
+	s.write(famend)
+
+s.write('''</ul>
+					</div>
+					</li>
+				</ul>			
+			</div><!--Close Package Header Div-->
+
+		</div><!--Close Container Div-->
+		</div>
+		</div>
+	</body>
+	
+</html>''')
+s.close()
+
+
+	p.close()
+	exit
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #	naming format = gen [species] price .JPG
 
 #		eua!baioensis!12.JPG
-#		coa!cinerea!45.JPG
+#		decy(
 
-
+'''
 
 def decy(filename):
-	filenamelist=filename.replace('.JPG', '').split('!')
-	for dic in plantindex:
-		for key in dic:
-			if filenamelist[0] in key:
-				filenamelist[0] = plantindex[plantindex.index(dic)].get(filenamelist[0])
-	return filenamelist
+	splitname=filename.replace('.JPG', '').split('!')
+	for key in plantindex[splitname[0]]:
+			if splitname[1] in key:
+				splitname[0] = plantindex[splitname[0]].get(splitname[1])
+	return splitname
+
+def devowel(word):
+	x = word.replace('a', '').replace('e', '').replace('i', '').replace('o', '').replace('u', '')
+	return x
 
 
 
 
-'''
-for entry in fam:
-...     for entry2 in fam.get(entry)[1]:
-...             if entry2 == 'Lit':
-...                      print(fam.get(entry)[1].get(entry2))
-'''
-def famcheck(fam):
-	list = []
+
+def abrgen(fam):
+	new = {}
 	for line in fam:
-		x = line.lower().replace(' ', '')
+		x = devowel(line.lower().replace(' ', ''))
 		x = x[:2] + x[-1:]
-		if x not in list:
-			list.append(x)
+		if x not in new:
+			new[x]=line
 		else:
-			x
+			print(line)
 
+'''
