71a0f1e1b874338767c09e87f1c9b5429921c5b9
carlyd95 <dcarltondunlap@hotmail.com>
2018-10-14 23:13:05 -0400

    14/10

--- update.py
+++ update.py
@@ -107,7 +107,7 @@ def addtosite(dicform):
 	for key in occup:
 		html.write( fambegin + dicform[key][0][1:] + titleend)
 		x = decy(dicform[key][1])
-		html.write('<a href="' + (linkfix + x[4].replace('"', '&#34;')) + '">' + (x[1] + ' ' + x[2]) + '</a>&nbsp;&nbsp;' + x[3])
+		html.write('<a href="' + (linkfix + x[4].replace('"', '&#34;').replace('?', '%3F')) + '">' + (x[1] + ' ' + x[2]) + '</a>&nbsp;&nbsp;' + x[3])
 		for index in dicform[key]:
 			if dicform[key].index(index) > 1:
 				x = decy(index)
